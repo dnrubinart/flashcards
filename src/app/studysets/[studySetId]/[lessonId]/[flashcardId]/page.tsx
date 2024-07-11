42b20f62e0c3c69576fb8809fb6b77f5778a14e8
Rubinart <k.ivanov19981@gmail.com>
2024-07-11 16:26:15 +0300

    Added shuffle button

--- src/app/studysets/[studySetId]/[lessonId]/[flashcardId]/page.tsx
+++ src/app/studysets/[studySetId]/[lessonId]/[flashcardId]/page.tsx
@@ -1,5 +1,7 @@
+"use client";
+import { useState } from "react";
 import Link from "next/link";
-import { FaArrowLeft } from "react-icons/fa";
+import { FaArrowLeft, FaRandom } from "react-icons/fa";
 import Flashcard from "../../../../../components/Flashcard";
 import { lessonData } from "../../../../../data/lessons/index";
 
@@ -11,17 +13,30 @@ interface FlashcardPageProps {
   };
 }
 
+function shuffle(array: any[]) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
+
 export default function FlashcardPage({ params }: FlashcardPageProps) {
   const { studySetId, lessonId, flashcardId } = params;
   const studySet = lessonData.filter((set) => set.slug === studySetId)[0];
   const lesson = studySet.data.filter((lesson) => lesson.slug === lessonId)[0];
   const unit = lesson.units.filter((unit) => unit.slug === flashcardId)[0];
 
+  const [cardData, setCardData] = useState(unit.items);
+
+  const handleShuffle = () => {
+    setCardData(shuffle([...cardData]));
+  };
+
   return (
     <div className="w-full rounded-lg bg-black border border-emerald-900 p-4 lg:p-6">
       <div className="mb-12 space-y-5">
         <div className="space-y-1.5">
-          {/* Back link */}
           <Link
             href={`/studysets/${studySetId}`}
             className="flex items-center gap-2 text-lg font-semibold text-emerald-400 hover:text-emerald-300 custom-transition"
@@ -29,12 +44,19 @@ export default function FlashcardPage({ params }: FlashcardPageProps) {
             <FaArrowLeft /> Back
           </Link>
           <h1 className="text-2xl sm:text-4xl font-bold">{unit.title}</h1>
-          <p className="text-neutral-400 font-semibold">
-          </p>
+          <p className="text-neutral-400 font-semibold"></p>
         </div>
 
-        {/* Flashcard Component */}
-        <Flashcard cardData={unit.items} />
+        <div className="space-y-5">
+          <button
+            onClick={() => handleShuffle()}
+            className="flex items-center gap-2 text-lg font-semibold text-emerald-400 hover:text-emerald-300 custom-transition"
+          >
+            <FaRandom className="text-emerald-400" />
+            <span className="ml-2">Shuffle</span>
+          </button>
+          <Flashcard cardData={cardData} />
+        </div>
       </div>
     </div>
   );
